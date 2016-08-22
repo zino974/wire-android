eda489d235f1e062edfb9f4bad0c32820eec6350
deanrobertcook <deanrobertcook@gmail.com>
2016-08-22 15:44:59 +0200

    WIP Add test class for scroll controller and test changing conversation scrolls to last read

--- app/src/main/scala/com/waz/zclient/controllers/context/ScrollController.scala
+++ app/src/main/scala/com/waz/zclient/controllers/context/ScrollController.scala
@@ -19,10 +19,10 @@ package com.waz.zclient.controllers.context
 
 import com.waz.ZLog.{LogTag, debug, logTagFor}
 import com.waz.model.ConvId
-import com.waz.service.ZMessaging
 import com.waz.utils.events.Signal
 import com.waz.zclient.controllers.global.KeyboardController
 import com.waz.zclient.{Injectable, Injector, WireContext}
+import timber.log.Timber
 
 class ScrollController(implicit cxt: WireContext, inj: Injector) extends Injectable {
 
@@ -30,38 +30,50 @@ class ScrollController(implicit cxt: WireContext, inj: Injector) extends Injecta
 
   import ScrollController._
 
-  val zms = inject[Signal[ZMessaging]]
   val keyboardVisibility = inject[KeyboardController].keyboardVisibility
+  val adapter = Signal[Adapter]
 
-  private var currentSelectedConv = Option.empty[ConvId]
-  val selectedConv = zms.flatMap(_.convsStats.selectedConversationId).collect { case Some(convId) => convId }
+  val lastReadIndex = adapter.flatMap(_.lastReadIndex)
 
-  val msgCursor = Signal(zms, selectedConv).flatMap {
-    case (z, convId) => z.messagesStorage.getEntries(convId)
-  }
-
-  val lastReadIndex = msgCursor.map(_.lastReadIndex)
-
-  val lastMessageIndex = msgCursor.map(_.size - 1)
+  val lastMessageIndex = adapter.flatMap(_.msgCount).map(_ - 1)
 
   val lastVisibleItemIndex = Signal(-1)
+
+  val selectedConv = adapter.flatMap(_.selectedConversation)
 
   val scrolledToBottom = lastVisibleItemIndex.zip(lastMessageIndex).map {
     case (lv, lm) if lv == lm => true
     case _ => false
   }
 
-  private var currentScrollPos = -1
+  @volatile private var currentScrollPos = -1
+  private var currentConv = Option.empty[ConvId]
+
   val scrollPosition = for {
+    conv <- selectedConv
     sB <- scrolledToBottom
     lm <- lastMessageIndex
-    kB <- keyboardVisibility
+    lr <- lastReadIndex
   } yield {
-    debug(s"ScrolledToBottom?: $sB, total count: $lm, keyboardVisible?: $kB")
-    if (sB) lm
-    else currentScrollPos // not scrolled to bottom
+    Timber.d(s"ScrolledToBottom?: $sB, total count: $lm")
+    if (sB) lm //keep scrolled to bottom
+    else {
+      if (!currentConv.contains(conv)) {
+        currentConv = Some(conv)
+        lr
+      }
+      else currentScrollPos
+    }
   }
   scrollPosition (currentScrollPos = _)
+}
+
+trait Adapter {
+  def lastReadIndex: Signal[Int]
+
+  def msgCount: Signal[Int]
+
+  def selectedConversation: Signal[ConvId]
 }
 
 object ScrollController {
