a82e8feca25bc190bc824a9ea6274624608c6009
deanrobertcook <deanrobertcook@gmail.com>
2016-08-30 13:53:16 +0200

    Change ProgressDots to drawable and use it as part of the asset background

--- app/src/main/scala/com/waz/zclient/messages/parts/AssetPartView.scala
+++ app/src/main/scala/com/waz/zclient/messages/parts/AssetPartView.scala
@@ -21,10 +21,11 @@ import java.util.Locale
 
 import android.animation.ValueAnimator
 import android.content.Context
+import android.graphics._
 import android.graphics.drawable.Drawable
-import android.graphics.{Canvas, ColorFilter, Paint, PixelFormat}
+import android.graphics.drawable.Drawable.Callback
 import android.util.AttributeSet
-import android.widget.{ImageView, LinearLayout}
+import android.widget.LinearLayout
 import com.waz.api
 import com.waz.api.AssetStatus._
 import com.waz.api.Message
@@ -32,23 +33,30 @@ import com.waz.api.impl.ProgressIndicator.ProgressData
 import com.waz.model.{AssetId, MessageContent, MessageData}
 import com.waz.service.ZMessaging
 import com.waz.threading.Threading
-import com.waz.utils.events.{EventContext, Signal}
+import com.waz.utils.events.{EventContext, EventStream, Signal}
+import com.waz.zclient.messages.parts.DeliveryState._
 import com.waz.zclient.messages.{MessageViewPart, MsgPart}
 import com.waz.zclient.ui.utils.TypefaceUtils
+import com.waz.zclient.utils.ViewUtils
 import com.waz.zclient.views.GlyphProgressView
 import com.waz.zclient.{Injectable, Injector, R, ViewHelper}
 
 class AssetPartView(context: Context, attrs: AttributeSet, style: Int) extends LinearLayout(context, attrs, style) with MessageViewPart with ViewHelper {
   def this(context: Context, attrs: AttributeSet) = this(context, attrs, 0)
-
   def this(context: Context) = this(context, null, 0)
 
   private lazy val assetActionButton: AssetActionButton = findById(R.id.action_button)
 
+  val assets = inject[AssetController]
+  val message = Signal[MessageData]()
+
+  setBackground(new AssetBackground(context, message.flatMap(m => assets.deliveryState(m))))
+
   override val tpe: MsgPart = MsgPart.Asset
 
   override def set(pos: Int, msg: MessageData, part: Option[MessageContent], widthHint: Int): Unit = {
-    assetActionButton.msgData ! msg
+    message ! msg
+    assetActionButton.message ! msg
   }
 }
 
@@ -57,17 +65,78 @@ class AssetController(implicit inj: Injector) extends Injectable {
 
   def assetSignal(id: AssetId) = assets.flatMap(_.assetSignal(id))
 
+  def deliveryState(m: MessageData) = assetSignal(m.assetId).map(a => DeliveryState(a._2, m.state))
+
   def downloadProgress(id: AssetId) = assets.flatMap(_.downloadProgress(id))
 
   def uploadProgress(id: AssetId) = assets.flatMap(_.uploadProgress(id))
 }
 
-class ProgressDotsView(context: Context, attrs: AttributeSet, style: Int) extends ImageView(context, attrs, style) with ViewHelper {
-  def this(context: Context, attrs: AttributeSet) = this(context, attrs, 0)
+protected sealed trait DeliveryState
 
-  def this(context: Context) = this(context, null, 0)
+protected object DeliveryState {
 
+  case object Complete extends DeliveryState
+
+  case object OtherUploading extends DeliveryState
+
+  case object Uploading extends DeliveryState
+
+  case object Downloading extends DeliveryState
+
+  case object Failed extends DeliveryState
+
+  case object Unknown extends DeliveryState
+
+  def apply(as: api.AssetStatus, ms: Message.Status): DeliveryState = (as, ms) match {
+    case (UPLOAD_CANCELLED | UPLOAD_FAILED | DOWNLOAD_FAILED, _) => Failed
+    case (UPLOAD_NOT_STARTED | META_DATA_SENT | PREVIEW_SENT | UPLOAD_IN_PROGRESS, mState) =>
+      mState match {
+        case Message.Status.FAILED => Failed
+        case Message.Status.SENT => OtherUploading
+        case _ => Uploading
+      }
+    case (DOWNLOAD_IN_PROGRESS, _) => Downloading
+    case (UPLOAD_DONE | DOWNLOAD_DONE, _) => Complete
+    case _ => Unknown
+  }
+}
+
+protected class AssetBackground(context: Context, deliveryState: Signal[DeliveryState])(implicit eventContext: EventContext) extends Drawable with Drawable.Callback {
+
+  private val cornerRadius = ViewUtils.toPx(context, 4).toFloat
+
+  private val backgroundPaint = new Paint
+  backgroundPaint.setColor(context.getResources.getColor(R.color.light_graphite_8))
+
+  private val dots = new ProgressDots(context, deliveryState.map { case OtherUploading => true; case _ => false }.onChanged)
+  dots.setCallback(this)
+
+  override def getCallback: Callback = super.getCallback
+
+  override def draw(canvas: Canvas): Unit = {
+    canvas.drawRoundRect(new RectF(getBounds), cornerRadius, cornerRadius, backgroundPaint)
+    dots.draw(canvas)
+  }
+
+  override def onBoundsChange(bounds: Rect): Unit = dots.setBounds(bounds)
+
+  override def setColorFilter(colorFilter: ColorFilter): Unit = ()
+
+  override def setAlpha(alpha: Int): Unit = ()
+
+  override def getOpacity: Int = PixelFormat.TRANSLUCENT
+
+  override def scheduleDrawable(who: Drawable, what: Runnable, when: Long): Unit = scheduleSelf(what, when)
+
+  override def invalidateDrawable(who: Drawable): Unit = invalidateSelf()
+
+  override def unscheduleDrawable(who: Drawable, what: Runnable): Unit = unscheduleSelf(what)
+}
+
+class ProgressDots(context: Context, showProgress: EventStream[Boolean])(implicit eventContext: EventContext) extends Drawable {
   private val ANIMATION_DURATION = 350 * 3
+
   private val lightPaint = new Paint
   private val darkPaint = new Paint
   private val dotSpacing = context.getResources.getDimensionPixelSize(R.dimen.progress_dot_spacing_and_width)
@@ -79,32 +148,34 @@ class ProgressDotsView(context: Context, attrs: AttributeSet, style: Int) extend
   darkPaint.setColor(context.getResources.getColor(R.color.graphite_40))
   animator.setRepeatCount(ValueAnimator.INFINITE)
   animator.setInterpolator(null)
+
   animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
     def onAnimationUpdate(animation: ValueAnimator): Unit = {
       darkDotIndex = animation.getAnimatedValue.asInstanceOf[Int]
-      invalidate()
+      invalidateSelf()
     }
   })
 
-  override def onAttachedToWindow(): Unit = {
-    super.onAttachedToWindow
-    animator.start()
+  showProgress.on(Threading.Ui) {
+    case true => animator.start()
+    case false => animator.cancel()
   }
 
-  override def onDetachedFromWindow(): Unit = {
-    super.onDetachedFromWindow
-    animator.cancel()
-  }
-
-  protected override def onDraw(canvas: Canvas): Unit = {
-    val centerX: Int = getWidth / 2
-    val centerY: Int = getHeight / 2
-    val dotLeftCenterX: Int = centerX - dotSpacing - dotRadius
-    val dotRightCenterX: Int = centerX + dotSpacing + dotRadius
+  override def draw(canvas: Canvas): Unit = if (animator.isRunning) {
+    val centerX = getBounds.width() / 2
+    val centerY = getBounds.height() / 2
+    val dotLeftCenterX = centerX - dotSpacing - dotRadius
+    val dotRightCenterX = centerX + dotSpacing + dotRadius
     canvas.drawCircle(dotLeftCenterX, centerY, dotRadius, if (darkDotIndex == 0) darkPaint else lightPaint)
     canvas.drawCircle(centerX, centerY, dotRadius, if (darkDotIndex == 1) darkPaint else lightPaint)
     canvas.drawCircle(dotRightCenterX, centerY, dotRadius, if (darkDotIndex == 2) darkPaint else lightPaint)
   }
+
+  override def setColorFilter(colorFilter: ColorFilter): Unit = ()
+
+  override def setAlpha(alpha: Int): Unit = ()
+
+  override def getOpacity: Int = PixelFormat.TRANSLUCENT
 }
 
 class AssetActionButton(context: Context, attrs: AttributeSet, style: Int) extends GlyphProgressView(context, attrs, style) with ViewHelper {
@@ -115,55 +186,31 @@ class AssetActionButton(context: Context, attrs: AttributeSet, style: Int) exten
 
   val assetService = inject[AssetController]
 
-  val msgData = Signal[MessageData]()
-  val msgState = msgData.map(_.state)
-  val assetStatus = msgData.flatMap(m => assetService.assetSignal(m.assetId)).map(_._2)
+  val message = Signal[MessageData]()
+  val deliveryState = message.flatMap(m => assetService.deliveryState(m))
 
-  private var overrideGlyph: String = null
+  def normalButtonBackground = context.getResources.getDrawable(R.drawable.selector__icon_button__background__video_message)
 
-  Signal(assetStatus, msgState).map { vals =>
-    import api.AssetStatus._
+  def errorButtonBackground = context.getResources.getDrawable(R.drawable.selector__icon_button__background__video_message__error)
 
-    def normalButtonBackground = context.getResources.getDrawable(R.drawable.selector__icon_button__background__video_message)
-    def errorButtonBackground = context.getResources.getDrawable(R.drawable.selector__icon_button__background__video_message__error)
-    def fileDrawable = new FileDrawable(context, msgData.map(_.assetId))
+  def fileDrawable = new FileDrawable(context, message.map(_.assetId))
 
-    vals match {
-      case (UPLOAD_CANCELLED, _) => (R.string.glyph__close, normalButtonBackground)
-
-      case (UPLOAD_FAILED, Message.Status.SENT) => (R.string.glyph__play, errorButtonBackground)
-      case (UPLOAD_FAILED, _) => (R.string.glyph__redo, errorButtonBackground)
-
-      case (UPLOAD_NOT_STARTED | META_DATA_SENT | PREVIEW_SENT | UPLOAD_IN_PROGRESS, mState) =>
-        mState match {
-          case Message.Status.FAILED => (R.string.glyph__redo, errorButtonBackground)
-          case Message.Status.SENT => (R.string.glyph__play, normalButtonBackground) //upload progress!
-          case _ => (R.string.glyph__close, normalButtonBackground) //upload progress
-        }
-
-      case (UPLOAD_DONE | DOWNLOAD_DONE, mState) => (0, fileDrawable)
-
-      case (DOWNLOAD_FAILED, _) => (R.string.glyph__redo, errorButtonBackground)
-      case (DOWNLOAD_IN_PROGRESS, _) => (R.string.glyph__close, normalButtonBackground) // download
-      case _ => (0, null)
-    }
-  }.map {
-    case (0, d) => ("", d)
-    case (id, d) => (context.getString(id), d)
+  //TODO playback controls for audio messages
+  deliveryState.map {
+    case Complete => (0, fileDrawable) //TODO handle audio and video messages here
+    case Uploading |
+         Downloading => (R.string.glyph__close, normalButtonBackground)
+    case Failed => (R.string.glyph__redo, errorButtonBackground)
+    case _ => (0, null)
   }.on(Threading.Ui) {
-    case (a, d) =>
-      setText(a)
-      setBackground(d)
+    case (action, drawable) =>
+      setText(if (action == 0) "" else context.getString(action))
+      setBackground(drawable)
   }
 
-  Signal(assetStatus, msgData).flatMap {
-    case (UPLOAD_NOT_STARTED | META_DATA_SENT | PREVIEW_SENT | UPLOAD_IN_PROGRESS, mData) =>
-      mData.state match {
-        case Message.Status.FAILED => Signal.const[Option[ProgressData]](None)
-        case Message.Status.SENT => assetService.uploadProgress(mData.assetId).map(Option(_))
-        case _ => assetService.uploadProgress(mData.assetId).map(Option(_))
-      }
-    case (DOWNLOAD_IN_PROGRESS, mData) => assetService.downloadProgress(mData.assetId).map(Option(_))
+  deliveryState.zip(message.map(_.assetId)).flatMap {
+    case (Uploading, id) => assetService.uploadProgress(id).map(Option(_))
+    case (Downloading, id) => assetService.downloadProgress(id).map(Option(_))
     case _ => Signal.const[Option[ProgressData]](None)
   }.on(Threading.Ui) {
     case Some(p) =>
@@ -176,8 +223,6 @@ class AssetActionButton(context: Context, attrs: AttributeSet, style: Int) exten
       }
     case _ => clearProgress()
   }
-
-  //TODO playback controls for audio message types
 }
 
 protected class FileDrawable(context: Context, assetId: Signal[AssetId])(implicit injector: Injector, cxt: EventContext) extends Drawable with Injectable {
